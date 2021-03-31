b374fa7c11446e0a9a942a518fd8f83a1a3e8974
盛光晟 <singlesaulwork@gmail.com>
2021-03-31 23:14:08 +0800

    update

--- src/CustomClass.js
+++ src/CustomClass.js
@@ -2,8 +2,8 @@ import React from 'react'
 import './custom.css'
 
 class CustomClass extends React.Component {
-    constructor(){
-        super()
+    constructor(props){
+        super(props)
         this.state ={
             m: 0,
             n: 0,
