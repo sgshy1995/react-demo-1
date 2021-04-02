c1660ee274b8b117cf03f80f29920d2db1ed49f9
盛光晟 <singlesaulwork@gmail.com>
2021-04-02 11:10:24 +0800

    custom hooks

--- src/CustomFunction.js
+++ src/CustomFunction.js
@@ -1,28 +1,50 @@
-import React from 'react'
-import './custom.css'
+import React, {useState, useEffect} from "react"
+import "./custom.css"
 
 function CustomFunction(props) {
-    const [state, setState] = React.useState({
-        n:0,
-        m:0
+    const [state, setState] = useState({
+        n: 0,
+        m: 0
     })
+
+    /**
+     * 自定义 hook
+     * 第一次渲染不更新
+     * */
+    const useUpdate = (fn, dependency) => {
+        const [count, setCount] = useState(0)
+        useEffect(() => {
+            setCount(count => count + 1)
+        }, [...dependency])
+        useEffect(() => {
+            if (count > 1) {
+                fn()
+            }
+        }, [count,fn])
+    }
+    useUpdate(() => {
+        console.log("m变化了------------", state.m)
+    }, [state.m])
+
     return (
         <div className="custom">
             React Function Create
-            <hr />
-            {props.numbers.join('-')}
-            <br />
+            <hr/>
+            {props.numbers.join("-")}
+            <br/>
             n: {state.n}
-            <br />
+            <br/>
             <button onClick={() => {
-                setState({...state,n:state.n+1})
-            }}>+1</button>
-            <br />
-             m: {state.m}
-            <br />
+                setState({...state, n: state.n + 1})
+            }}>+1
+            </button>
+            <br/>
+            m: {state.m}
+            <br/>
             <button onClick={() => {
-                setState({...state,m:state.m+1})
-            }}>+1</button>
+                setState({...state, m: state.m + 1})
+            }}>+1
+            </button>
         </div>
     )
 }
